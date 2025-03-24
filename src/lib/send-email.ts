c3b07e979dbba9c08f4233201cc6a0890de3f072
Oleksandr Zariechniev <158515671+ozariechniev@users.noreply.github.com>
2025-03-24 22:48:55 +0100

    Remove comment string

--- src/lib/send-email.ts
+++ src/lib/send-email.ts
@@ -3,7 +3,7 @@ import nodemailer from 'nodemailer';
 export const transporter = nodemailer.createTransport({
   host: process.env.NODEMAILER_HOST,
   port: Number(process.env.NODEMAILER_PORT),
-  secure: Boolean(process.env.NODEMAILER_SECURE), // true for port 465, false for other ports
+  secure: Boolean(process.env.NODEMAILER_SECURE),
   auth: {
     user: process.env.NODEMAILER_USER,
     pass: process.env.NODEMAILER_PASS,
