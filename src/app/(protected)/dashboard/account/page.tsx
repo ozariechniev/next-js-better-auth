5476fdac5b9ee8cc5271d792c2c1075753408006
Oleksandr Zariechniev <158515671+ozariechniev@users.noreply.github.com>
2025-03-26 01:22:43 +0100

    Update Account pages skeleton

--- src/app/(protected)/dashboard/account/page.tsx
+++ src/app/(protected)/dashboard/account/page.tsx
@@ -4,8 +4,14 @@ export default async function AccountPage() {
   await getUser();
 
   return (
-    <div className="px-4 py-8">
+    <div className="flex flex-1 flex-col gap-4 p-4">
       <h1 className="text-2xl">Account</h1>
+      <div className="grid auto-rows-min gap-4 md:grid-cols-3">
+        <div className="bg-muted/50 aspect-video rounded-xl" />
+        <div className="bg-muted/50 aspect-video rounded-xl" />
+        <div className="bg-muted/50 aspect-video rounded-xl" />
+      </div>
+      <div className="bg-muted/50 min-h-[100vh] flex-1 rounded-xl md:min-h-min" />
     </div>
   );
 }
