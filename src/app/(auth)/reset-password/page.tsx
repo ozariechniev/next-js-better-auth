162ea878bcdcc5b90a362ed0b5cdc7c1d7e64475
Oleksandr Zariechniev <158515671+ozariechniev@users.noreply.github.com>
2025-03-26 15:31:37 +0100

    Add ResetPasswordFormFallback component and implement Suspense in ResetPasswordPage

--- src/app/(auth)/reset-password/page.tsx
+++ src/app/(auth)/reset-password/page.tsx
@@ -1,11 +1,15 @@
+import { Suspense } from 'react';
 import { AuthWelcome } from '../_components/auth-welcome';
 import { ResetPasswordForm } from '../_components/reset-password-form';
+import { ResetPasswordFormFallback } from '../_components/reset-password-form-fallback';
 
 export default function ResetPasswordPage() {
   return (
     <>
       <AuthWelcome title="Welcome" description="Reset your account password" />
-      <ResetPasswordForm />
+      <Suspense fallback={<ResetPasswordFormFallback />}>
+        <ResetPasswordForm />
+      </Suspense>
     </>
   );
 }
