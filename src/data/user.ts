36f6389e3e912843cc76fc7c99a69c7c46013952
Oleksandr Zariechniev <158515671+ozariechniev@users.noreply.github.com>
2025-03-28 01:33:58 +0100

    Refactor user session handling to use updated user schema and session type

--- src/data/user.ts
+++ src/data/user.ts
@@ -1,27 +1,30 @@
-'use server';
-
+import 'server-only';
 import { cache } from 'react';
 import { headers } from 'next/headers';
 import { redirect } from 'next/navigation';
 import { auth } from '@/lib/auth';
 import { ACCESS_DENIED_URL } from '@/lib/constants';
-import { userDTOSchema } from '@/lib/definitions';
+import { userRoleEnum, userSchema } from '@/lib/definitions';
+import { Session } from '@/lib/types';
 
 export const getUser = cache(async () => {
-  const session = await auth.api.getSession({
+  const session: Session | null = await auth.api.getSession({
     headers: await headers(),
   });
 
-  if (!session?.user) {
+  if (!session) {
     return null;
   }
 
-  const result = userDTOSchema.safeParse({
-    name: session.user.name,
-    email: session.user.email,
-    emailVerified: session.user.emailVerified,
-    image: session.user.image,
-    role: session.user.role,
+  const { user, session: userSession } = session;
+
+  const result = userSchema.safeParse({
+    name: user.name,
+    email: user.email,
+    emailVerified: user.emailVerified,
+    image: user.image,
+    role: user.role,
+    sessionId: userSession.id,
   });
 
   if (!result.success) {
@@ -37,12 +40,16 @@ export const requireUser = async () => {
   if (!user) {
     redirect(ACCESS_DENIED_URL);
   }
+
+  return user;
 };
 
 export const requireAdmin = async () => {
   const user = await getUser();
 
-  if (!user || user.role !== 'admin') {
+  if (!user || user.role !== userRoleEnum.enum.admin) {
     redirect(ACCESS_DENIED_URL);
   }
+
+  return user;
 };
