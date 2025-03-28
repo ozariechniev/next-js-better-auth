7f43a0c7d38cdd21ce1f737f6b2ba0f992c5d060
Oleksandr Zariechniev <158515671+ozariechniev@users.noreply.github.com>
2025-03-28 09:26:32 +0100

    Update UserProfile component to accept user prop

--- src/app/(protected)/dashboard/_components/profile/user-profile.tsx
+++ src/app/(protected)/dashboard/_components/profile/user-profile.tsx
@@ -1,32 +1,37 @@
 'use client';
 
-import { CheckCircle, User } from 'lucide-react';
+import Image from 'next/image';
+import { BadgeAlert, BadgeCheck, UserIcon } from 'lucide-react';
 import { Badge } from '@/components/ui/badge';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import { User } from '@/lib/types';
 
-const user = {
-  name: 'John Doe',
-  verified: true,
-  email: 'test@test.com',
-};
-
-export function UserProfile() {
+export function UserProfile({ user }: { user: User }) {
   return (
     <Card className="font-mono">
       <CardHeader>
         <CardTitle>Personal Information</CardTitle>
       </CardHeader>
       <CardContent className="flex flex-col items-center space-y-4">
-        <div className="bg-muted relative h-24 w-24 overflow-hidden rounded-full">
-          <User className="h-full w-full p-4" />
+        <div className="bg-muted relative size-24 overflow-hidden rounded-full">
+          {user.image ? (
+            <Image src={user.image} alt={user.name} layout="fill" objectFit="cover" />
+          ) : (
+            <UserIcon className="h-full w-full p-4" />
+          )}
         </div>
         <div className="text-center">
           <h2 className="mb-4 text-xl font-semibold">{user.name}</h2>
           <div className="flex items-center justify-center space-x-2">
             <span className="text-muted-foreground text-sm">{user.email}</span>
-            {user.verified && (
+            {user.emailVerified ? (
               <Badge className="bg-green-500 text-white">
-                <CheckCircle className="mr-1 h-3 w-3" /> Verified
+                <BadgeCheck className="mr-1 size-5!" /> Verified
+              </Badge>
+            ) : (
+              <Badge className="bg-orange-500 text-white">
+                <BadgeAlert className="mr-1 size-5!" />
+                Unverified
               </Badge>
             )}
           </div>
--- src/app/(protected)/dashboard/profile/page.tsx
+++ src/app/(protected)/dashboard/profile/page.tsx
@@ -4,14 +4,14 @@ import { UserSessions } from '../_components/profile/user-sessions';
 import { UserSettings } from '../_components/profile/user-settings';
 
 export default async function AccountPage() {
-  await requireUser();
+  const user = await requireUser();
 
   return (
     <div className="p-4 lg:p-6">
       <h1 className="mb-5 text-2xl">User Profile</h1>
       <div className="@container">
         <div className="mb-5 grid grid-cols-1 gap-6 overflow-hidden @lg:grid-cols-[3fr_1fr]">
-          <UserProfile />
+          <UserProfile user={user} />
           <UserSettings />
         </div>
         <UserSessions />
