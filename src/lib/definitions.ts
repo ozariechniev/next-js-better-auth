c4a18cd5c2d052f10670f1cd01dd8c8ea85d1eb4
Oleksandr Zariechniev <158515671+ozariechniev@users.noreply.github.com>
2025-03-28 01:27:31 +0100

    Refactor validation schemas to improve readability and maintainability

--- src/lib/definitions.ts
+++ src/lib/definitions.ts
@@ -1,54 +1,86 @@
 import { z } from 'zod';
 
+const getNameSchema = () =>
+  z.string().min(1, { message: 'Name is required' }).max(50, { message: 'Name must be less than 50 characters' });
+const getEmailSchema = () => z.string().email({ message: 'Invalid email address' });
+const getPasswordSchema = () => z.string().min(8, { message: 'Password must be at least 8 characters long' });
+const getConfirmPasswordSchema = () =>
+  z.string().min(8, { message: 'Confirm password is required' }).max(128, {
+    message: 'Confirm password must be less than 128 characters',
+  });
+const getRememberMeSchema = () => z.boolean().default(false).optional();
+
+const USER_ROLE = ['user', 'admin'] as const;
+
+/**
+ * ----------------------------------------------------------------------------
+ * Sign In Schema
+ * ----------------------------------------------------------------------------
+ */
+
 export const signInSchema = z.object({
-  email: z.string().email({ message: 'Invalid email address' }),
-  password: z.string().min(8, { message: 'Password must be at least 8 characters long' }),
-  rememberMe: z.boolean().default(false).optional(),
+  email: getEmailSchema(),
+  password: getPasswordSchema(),
+  rememberMe: getRememberMeSchema(),
 });
+
+/**
+ * ----------------------------------------------------------------------------
+ * Sign Up Schema
+ * ----------------------------------------------------------------------------
+ */
 
 export const signUpSchema = z
   .object({
-    name: z
-      .string()
-      .min(1, { message: 'Name is required' })
-      .max(50, { message: 'Name must be less than 50 characters' }),
-    email: z.string().email({ message: 'Invalid email address' }),
-    password: z
-      .string()
-      .min(8, { message: 'Password must be at least 8 characters long' })
-      .max(128, { message: 'Password must be less than 128 characters' }),
-    confirmPassword: z.string().min(8, { message: 'Confirm password is required' }).max(128, {
-      message: 'Confirm password must be less than 128 characters',
-    }),
+    name: getNameSchema(),
+    email: getEmailSchema(),
+    password: getPasswordSchema(),
+    confirmPassword: getConfirmPasswordSchema(),
   })
   .refine((data) => data.password === data.confirmPassword, {
     path: ['confirmPassword'],
     message: 'Passwords do not match',
   });
 
+/**
+ * ----------------------------------------------------------------------------
+ * Forgot Password Schema
+ * ----------------------------------------------------------------------------
+ */
+
 export const forgotPasswordSchema = z.object({
-  email: z.string().email({ message: 'Invalid email address' }),
+  email: getEmailSchema(),
 });
+
+/**
+ * ----------------------------------------------------------------------------
+ * Reset Password Schema
+ * ----------------------------------------------------------------------------
+ */
 
 export const resetPasswordSchema = z
   .object({
-    password: z
-      .string()
-      .min(8, { message: 'Password must be at least 8 characters long' })
-      .max(128, { message: 'Password must be less than 128 characters' }),
-    confirmPassword: z.string().min(8, { message: 'Confirm password is required' }).max(128, {
-      message: 'Confirm password must be less than 128 characters',
-    }),
+    password: getPasswordSchema(),
+    confirmPassword: getConfirmPasswordSchema(),
   })
   .refine((data) => data.password === data.confirmPassword, {
     path: ['confirmPassword'],
     message: 'Passwords do not match',
   });
 
-export const userDTOSchema = z.object({
+/**
+ * ----------------------------------------------------------------------------
+ * User Schema
+ * ----------------------------------------------------------------------------
+ */
+
+export const userRoleEnum = z.enum(USER_ROLE);
+
+export const userSchema = z.object({
   name: z.string(),
   email: z.string(),
   emailVerified: z.boolean(),
   image: z.string().nullable(),
-  role: z.enum(['user', 'admin']),
+  role: userRoleEnum,
+  sessionId: z.string(),
 });
