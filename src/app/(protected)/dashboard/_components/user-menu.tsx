240a649bd271474afbd60e6cd15740a328bd0383
Oleksandr Zariechniev <158515671+ozariechniev@users.noreply.github.com>
2025-04-01 22:58:56 +0200

    Add sign-out alert dialog action

--- src/app/(protected)/dashboard/_components/user-menu.tsx
+++ src/app/(protected)/dashboard/_components/user-menu.tsx
@@ -3,8 +3,22 @@
 import { useState } from 'react';
 import { useRouter } from 'next/navigation';
 import { usePathname } from 'next/navigation';
-import { ChevronsUpDown, LayoutDashboard, LogOut, Settings, UserIcon } from 'lucide-react';
+import { ErrorContext } from '@better-fetch/fetch';
+import { ChevronsUpDown, LayoutDashboard, Loader2, LogOut, Settings, UserIcon } from 'lucide-react';
+import { toast } from 'sonner';
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from '@/components/ui/alert-dialog';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+import { Button } from '@/components/ui/button';
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -31,47 +45,43 @@ export function UserMenu({ user }: { user: User }) {
   const pathname = usePathname();
   const { isMobile } = useSidebar();
   const [signingOut, setSigningOut] = useState(false);
+  const [open, setOpen] = useState(false);
 
   const onSignOut = () => {
-    setSigningOut(true);
-    authClient.signOut({
-      fetchOptions: {
-        onSuccess() {
-          router.push('/');
-        },
+    const fetchOptions = {
+      onResponse: () => {
+        setSigningOut(false);
       },
+      onRequest: () => {
+        setSigningOut(true);
+      },
+      onSuccess: () => {
+        setOpen(false);
+        router.push('/');
+        toast.success('Signed out successfully');
+      },
+      onError: (ctx: ErrorContext) => {
+        setSigningOut(false);
+        setOpen(false);
+        toast.error(ctx.error.message ?? 'Internal server error. Please, try again later.');
+      },
+    };
+
+    authClient.signOut({
+      fetchOptions,
     });
-    setSigningOut(false);
   };
 
   return (
-    <SidebarMenu>
-      <SidebarMenuItem>
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <SidebarMenuButton
-              size="lg"
-              className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
-            >
-              <Avatar className="h-8 w-8 rounded-lg">
-                <AvatarImage className="object-cover" src={user.image || undefined} alt={user.name} />
-                <AvatarFallback className="rounded-lg">{user.name.charAt(0)}</AvatarFallback>
-              </Avatar>
-              <div className="grid flex-1 text-left text-sm leading-tight">
-                <span className="truncate font-semibold">{user.name}</span>
-                <span className="truncate text-xs">{user.email}</span>
-              </div>
-              <ChevronsUpDown className="ml-auto size-4" />
-            </SidebarMenuButton>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent
-            className="w-[--radix-dropdown-menu-trigger-width] min-w-56 rounded-lg"
-            side={isMobile ? 'bottom' : 'right'}
-            align="end"
-            sideOffset={4}
-          >
-            <DropdownMenuLabel className="p-0 font-normal">
-              <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
+    <>
+      <SidebarMenu>
+        <SidebarMenuItem>
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <SidebarMenuButton
+                size="lg"
+                className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
+              >
                 <Avatar className="h-8 w-8 rounded-lg">
                   <AvatarImage className="object-cover" src={user.image || undefined} alt={user.name} />
                   <AvatarFallback className="rounded-lg">{user.name.charAt(0)}</AvatarFallback>
@@ -80,40 +90,94 @@ export function UserMenu({ user }: { user: User }) {
                   <span className="truncate font-semibold">{user.name}</span>
                   <span className="truncate text-xs">{user.email}</span>
                 </div>
-              </div>
-            </DropdownMenuLabel>
-            <DropdownMenuSeparator />
-            <DropdownMenuGroup>
-              <DropdownMenuItem
-                className={pathname === DASHBOARD_URL ? 'bg-sidebar-accent' : ''}
-                onClick={() => router.push(DASHBOARD_URL)}
-              >
-                <LayoutDashboard />
-                {DASHBOARD_LABEL}
-              </DropdownMenuItem>
-              <DropdownMenuItem
-                className={pathname.startsWith(DASHBOARD_PROFILE_URL) ? 'bg-sidebar-accent' : ''}
-                onClick={() => router.push(DASHBOARD_PROFILE_URL)}
-              >
-                <UserIcon />
-                {DASHBOARD_PROFILE_LABEL}
-              </DropdownMenuItem>
-              <DropdownMenuItem
-                className={pathname.startsWith(DASHBOARD_SETTINGS_URL) ? 'bg-sidebar-accent' : ''}
-                onClick={() => router.push(DASHBOARD_SETTINGS_URL)}
-              >
-                <Settings />
-                {DASHBOARD_SETTINGS_LABEL}
-              </DropdownMenuItem>
-            </DropdownMenuGroup>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem variant="destructive" disabled={signingOut} onClick={onSignOut}>
-              <LogOut />
-              Log out
-            </DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
-      </SidebarMenuItem>
-    </SidebarMenu>
+                <ChevronsUpDown className="ml-auto size-4" />
+              </SidebarMenuButton>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent
+              className="w-[--radix-dropdown-menu-trigger-width] min-w-56 rounded-lg"
+              side={isMobile ? 'bottom' : 'right'}
+              align="end"
+              sideOffset={4}
+            >
+              <DropdownMenuLabel className="p-0 font-normal">
+                <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
+                  <Avatar className="h-8 w-8 rounded-lg">
+                    <AvatarImage className="object-cover" src={user.image || undefined} alt={user.name} />
+                    <AvatarFallback className="rounded-lg">{user.name.charAt(0)}</AvatarFallback>
+                  </Avatar>
+                  <div className="grid flex-1 text-left text-sm leading-tight">
+                    <span className="truncate font-semibold">{user.name}</span>
+                    <span className="truncate text-xs">{user.email}</span>
+                  </div>
+                </div>
+              </DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuGroup>
+                <DropdownMenuItem
+                  className={pathname === DASHBOARD_URL ? 'bg-sidebar-accent' : ''}
+                  onClick={() => router.push(DASHBOARD_URL)}
+                >
+                  <LayoutDashboard />
+                  {DASHBOARD_LABEL}
+                </DropdownMenuItem>
+                <DropdownMenuItem
+                  className={pathname.startsWith(DASHBOARD_PROFILE_URL) ? 'bg-sidebar-accent' : ''}
+                  onClick={() => router.push(DASHBOARD_PROFILE_URL)}
+                >
+                  <UserIcon />
+                  {DASHBOARD_PROFILE_LABEL}
+                </DropdownMenuItem>
+                <DropdownMenuItem
+                  className={pathname.startsWith(DASHBOARD_SETTINGS_URL) ? 'bg-sidebar-accent' : ''}
+                  onClick={() => router.push(DASHBOARD_SETTINGS_URL)}
+                >
+                  <Settings />
+                  {DASHBOARD_SETTINGS_LABEL}
+                </DropdownMenuItem>
+              </DropdownMenuGroup>
+              <DropdownMenuSeparator />
+
+              <AlertDialog open={open} onOpenChange={setOpen}>
+                <AlertDialogTrigger asChild>
+                  <Button
+                    variant="ghost"
+                    className="text-destructive hover:text-destructive w-full justify-start"
+                    disabled={signingOut}
+                  >
+                    <LogOut />
+                    Sign Out
+                  </Button>
+                </AlertDialogTrigger>
+                <AlertDialogContent>
+                  <AlertDialogHeader>
+                    <AlertDialogTitle>Sign out</AlertDialogTitle>
+                    <AlertDialogDescription>To sign out, click the button below.</AlertDialogDescription>
+                  </AlertDialogHeader>
+                  <AlertDialogFooter>
+                    {!signingOut && <AlertDialogCancel>Cancel</AlertDialogCancel>}
+                    <AlertDialogAction>
+                      {signingOut ? (
+                        <span>
+                          <Loader2 className="h-4 w-4 animate-spin" />
+                        </span>
+                      ) : (
+                        <span
+                          onClick={(e) => {
+                            e.preventDefault();
+                            onSignOut();
+                          }}
+                        >
+                          Sign out
+                        </span>
+                      )}
+                    </AlertDialogAction>
+                  </AlertDialogFooter>
+                </AlertDialogContent>
+              </AlertDialog>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </SidebarMenuItem>
+      </SidebarMenu>
+    </>
   );
 }
